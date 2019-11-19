8851ce1444f83d7b4e2818aa4449a0035f5be51e
Artemmal <45432448+Artemmal@users.noreply.github.com>
2019-11-19 15:13:38 +0200

    Add files via upload

---
+++ Program(1).cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class ReverseComparer : IComparer
+    {
+
+        public int Compare(Object x, Object y)
+        {
+            return (new CaseInsensitiveComparer()).Compare(y, x);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100000000];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next();
+            }
+            IComparer revComparer = new ReverseComparer();
+            MissionImpossibleAsync();
+            Array.Sort(array);
+
+            
+
+            Console.WriteLine("Mission complete");
+        }
+
+        public static void BeepMethod()
+        {
+
+
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(554, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(523, 1200);
+            Thread.Sleep(150);
+            Console.Beep(466, 150);
+            Console.Beep(523, 150);
+
+        }
+
+        static async void MissionImpossibleAsync()
+        {
+            Console.WriteLine("Миссия невыполнима");
+
+            await Task.Run(()=>BeepMethod());
+        }
+    }
+}
